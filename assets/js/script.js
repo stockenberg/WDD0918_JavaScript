68955bab4bf676aa763d5cfa883dcae6a14765b5
Marten Stockenberg <MStockenberg@googlemail.com>
2019-01-15 12:30:14 +0100

    logs removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,37 +4,26 @@ var boxes = document.getElementsByClassName('lightbox');
 function generateLightbox(imgURL, imgDescription) {
     var div = document.createElement('div');
     div.id = "lightbox";
-    console.log('generated div#lightbox');
 
     div.appendChild(generatedImg(imgURL, imgDescription));
-    console.log('built figure');
     div.appendChild(generatedThumbnails());
-    console.log('generated thumbnails');
     // Write HTML to Document Body
     document.body.appendChild(div);
-    console.log('attached html to body');
 }
 
 
 function generatedImg(imgURL, imgDescription){
 
     var figure = document.createElement('figure');
-    console.log('created figure');
     var img = document.createElement('img');
-    console.log('created img');
     img.src = imgURL;
     img.alt = imgDescription;
-    console.log('src and alt was setted');
 
     var figcaption = document.createElement('figcaption');
     figcaption.innerHTML = imgDescription;
-    console.log('added description to figcaption');
 
     figure.appendChild(img);
-    console.log('appended img');
     figure.appendChild(figcaption);
-    console.log('appended figcaption');
-    console.log('ready to return img with figure and figcaption');
     return figure;
 }
 
@@ -52,10 +41,8 @@ function generatedThumbnails() {
 function openLightbox(e) {
     var imgURL = e.target.src;
     var imgDescription = e.target.nextElementSibling.innerHTML;
-    console.log('got img url and description');
     // generate Lightbox here!
     generateLightbox(imgURL, imgDescription);
-    console.log("generation successfull");
 }
 
 // Lightbox loop
