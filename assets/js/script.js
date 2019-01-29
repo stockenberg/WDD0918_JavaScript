96950d1064c33b9f744c65a2dfaeffbd26c4da0d
Marten Stockenberg <MStockenberg@googlemail.com>
2019-01-29 12:12:03 +0100

    accordion single completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,9 @@
     elem.classList.toggle()
 */
 
+// Variables to store Elements, to check if the Elements are equal
+let currentElement;
+let previousElement;
 
 // Selektiere Buttons
 let btns = document.getElementsByClassName('btn-info');
@@ -12,10 +15,32 @@ console.log(btns);
 // Buttons brauchen ein Event
 for(let i = 0; i < btns.length; i++){
     btns[i].onclick = function (ev) {
-        // sibling-text hat eine Klasse und ist damit unsichtbar (display none)
-        console.log(ev);
-        // klasse muss ihm weg genommen werden
-        ev.target.nextElementSibling.classList.toggle('hidden');
-                // => Textfeld erscheint
+
+        // Store current Element
+        currentElement = ev.target;
+
+        // If first element matches last element
+        if(currentElement === previousElement){
+            console.log('elemente sind gleich');
+            // Toggle Accordion
+            ev.target.nextElementSibling.classList.toggle('visible');
+        }
+        // IF first element not equal last element
+        else{
+            console.log('elemente sind ungleich');
+            // Get all elements with visible class
+            let visible = document.getElementsByClassName('visible')[0];
+
+            if (typeof visible !== 'undefined'){
+                // remove visible class (accordion is closed)
+                visible.classList.remove('visible');
+                console.log('removed');
+            }
+            // open clicked accordion entry
+            ev.target.nextElementSibling.classList.add('visible');
+
+        }
+        // Store last element (in 2nd iteration)
+        previousElement = ev.target;
     }
 }
