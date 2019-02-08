2b3b613e687d36f300d1aef7adc58423b29f8b21
Marten Stockenberg <MStockenberg@googlemail.com>
2019-02-08 12:00:17 +0100

    form completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,90 @@
 let form = document.querySelector('form.col');
-console.dir(form);
 
+/**
+ * TODO:
+ * - Reset Red Color
+ * - Print error message in HTML
+ * - remove redundancies
+ */
 
-form[2].onblur = function (ev) {
-    // console.log('blured');   
-    console.log(form[2].value);
-    if(form[2].value.length === 0){
-       form[2].style.border = 'thin solid red';
-       console.log('Bitte fülle die E-Mail aus');
+let errorMessages = {
+    lastname: 'Bitte gib deinen Namen ein',
+    email: {
+        empty: 'Bitte fülle die E-Mail aus',
+        nomatch: 'Deine E-Mail-Adresse ist nicht korrekt'
     }
-}
+};
 
-form[1].onblur = function (ev){
-     // console.log('blured');   
-     console.log(form[1].value);
-     if(form[1].value.length === 0){
-        form[1].style.border = 'thin solid red';
-        console.log('Bitte gib deinen Namen ein');
-     } 
-}
-
-form[4].onclick = function (ev) {
-    console.log(ev);
-    if(ev.target.checked === false){
-        console.log('Bitte checke die AGB');
-    }
+/**
+ * gets and HTML element and an error message
+ * changes border on HTML-element and prints message in console
+ * @param {*} elem 
+ * @param {*} error 
+ */
+function setError(elem, error) {
+    elem.style.border = 'thin solid #ffafb4';
+    iziToast.show({
+        title: 'Error!',
+        message: error,
+        color: 'red',
+        position: 'topCenter'
+    });
+   
 }
 
 /**
- * obj = {firstname: value, lastname: value, email: value};
- * arr = ['Hans', 'wurst', 'hans@wurst.de'];
+ * Iteration over lastname and email field, validation
  */
+for (let i = 1; i < 3; i++) {
+    form[i].onblur = function (ev) {
+        
+        /** checks the given input:type */
+        switch (ev.target.type) {
+            /* If Input type is email - then validate email field */
+            case 'email':
+                let regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+                if (ev.target.value.length === 0) {
+                    setError(ev.target, errorMessages[ev.target.id].empty);
+                }else{
+                    if(ev.target.value.match(regexp)){
+                        ev.target.style.border = null;
+                    }else{
+                        setError(ev.target, errorMessages[ev.target.id].nomatch);
+                    }
+                }
+
+                break;
+            /** if the input type is anything else - validate empty */
+            default:
+                if (ev.target.value.length === 0) {            
+                    setError(ev.target, errorMessages[ev.target.id]);
+                }else{
+                    ev.target.style.border = null;
+                }
+                break;
+        }
+    }
+}
 
 
+/**
+ * Checks content from firstname and lastname.
+ * Checks checkbox if is enabled
+ */
 form.onsubmit = function (ev) {
     ev.preventDefault();
-    console.log('test');
-    console.log(ev);
+   
     if (
-        ev.target[1].value.length >= 2 && 
+        ev.target[1].value.length >= 2 &&
         ev.target[2].value.length >= 6 && ev.target[4].checked) {
-        console.log('formval completed - submit Data');
+            iziToast.show({
+                title: 'Success!',
+                message: 'Formular erfolgreich abgeschickt',
+                color: 'green',
+                position: 'topCenter'
+            });
     }
 
- 
+
 }
